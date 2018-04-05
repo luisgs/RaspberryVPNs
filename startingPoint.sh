44ce0f0d136ad2738811c2dd0400d2c18dba306a
luisgs <luis.ild@gmail.com>
2018-04-05 11:16:29 +0000

    Adding variables files that contains our future vars and the starting script file

---
+++ startingPoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  startingPoint.sh
+# 
+#         USAGE:  ./startingPoint.sh 
+# 
+#   DESCRIPTION:  This is the first and only scripts needs to be run. it will 
+#		  call the other scripts and ask you for the additional info.
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  LuisGS (), luis.ild
+#       COMPANY:  Home
+#       VERSION:  1.0
+#       CREATED:  04/05/18 11:07:11 UTC
+#      REVISION:  ---
+#===============================================================================
+
+
